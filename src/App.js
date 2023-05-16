df546a114357c44ea5ace083ebdecc717615c59f
Saijeet Upadhyay <saijeet2019@gmail.com>
2023-05-16 21:30:05 +0545

    complete : folder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Expenses from "./components/Expenses";
+import React from "react";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -18,9 +19,9 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenses item={expenses}/>
+      <Expenses item={expenses} />
     </div>
   );
-}
+};
 
 export default App;
