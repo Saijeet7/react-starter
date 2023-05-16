4d5e5d6c2248bbf23e360f5ad218b33b7b37be15
Saijeet Upadhyay <saijeet2019@gmail.com>
2023-05-16 20:47:20 +0545

    Complete : Expense component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import Expenses from "./components/Expenses";
+
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Iphone",
+      amount: "1000",
+      date: new Date(2020, 7, 1),
+    },
+    {
+      id: "e2",
+      title: "Macbook",
+      amount: "1200",
+      date: new Date(2023, 9, 10),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expenses item={expenses}/>
     </div>
   );
 }
